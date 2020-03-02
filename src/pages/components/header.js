38903c9c6130f5024aa33f6a586971039cc235e7
beata <beata@emc2wave.com>
2020-03-02 14:56:13 -0800

    change a to .link in header.scss

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -10,7 +10,9 @@ const Header = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link className="link" to="/">
+              Home
+            </Link>
           </li>
           <li>
             <Link to="/contact">Contact</Link>
