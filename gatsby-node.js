c86878e2813c0f80263d708d2a18d4ef27bd2c5f
beata <beata@emc2wave.com>
2020-03-06 11:25:15 -0800

    add slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,6 @@ module.exports.onCreateNode = ({ node, getNote, actions }) => {
   if (node.internal.type === "MarkdownRemark") {
     const slug = path.basename(node.fileAbsolutePath, ".md")
 
-    console.log(JSON.stringify(node, undefined, 4))
+    console.log("@@@@@@@@@@@@@@", slug)
   }
 }
