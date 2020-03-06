6c1083ef49553b78cd23338dc14c0162e9c7b358
beata <beata@emc2wave.com>
2020-03-06 11:16:51 -0800

    add onCreateNode

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,7 @@
-module.exports.onCreateNode = ({ node, actions }) => {
+module.exports.onCreateNode = ({ node, getNote, actions }) => {
   const { createNodeField } = actions
+
+  if (node.internal.type === "MarkdownRemark") {
+    console.log(JSON.stringify(node, undefined, 4))
+  }
 }
