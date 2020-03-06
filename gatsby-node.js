ffaccbf7e8e4b4056a49dc83cd583548956ca9ec
beata <beata@emc2wave.com>
2020-03-06 11:23:44 -0800

    add path.basename

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,11 @@
+const path = require("path")
+
 module.exports.onCreateNode = ({ node, getNote, actions }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === "MarkdownRemark") {
+    const slug = path.basename(node.fileAbsolutePath, ".md")
+
     console.log(JSON.stringify(node, undefined, 4))
   }
 }
