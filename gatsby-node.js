0f9bcfe44d401388f1415702026df98c4bf488c8
beata <beata@emc2wave.com>
2020-03-07 17:42:14 -0800

    add fetch data to createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,7 +15,7 @@ module.exports.onCreateNode = ({ node, getNote, actions }) => {
 }
 
 module.exports.createPages = async ({ graphql, actions }) => {
-  const { createPages } = actions
+  const { createPage } = actions
   const blogTemplate = path.resolve("./src/templates/blog.js")
   const res = await graphql(`
     query {
@@ -30,4 +30,14 @@ module.exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
+
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
 }
