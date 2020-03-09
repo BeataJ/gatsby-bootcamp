2563c2162fd3ae120b303964c72842509f6a2cf7
beata <beata@emc2wave.com>
2020-03-08 18:53:31 -0700

    add query to blog template like props

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,9 +1,26 @@
 import React from "react"
+import { graphql } from "graphql"
 
 import Layout from "../components/layout"
 
-const Blog = () => {
-  return <Layout>This is the blog template</Layout>
+export const query = graphql`
+  query($slug: String) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = props => {
+  return (
+    <Layout>
+      <h1>props.data.markdownRemark.frontmatter.title</h1>
+    </Layout>
+  )
 }
 
 export default Blog
