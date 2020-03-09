ad768b5e19a688994f25d3883c8fd31aa6fab41e
beata <beata@emc2wave.com>
2020-03-08 22:49:17 -0700

    add <p. with date

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -19,6 +19,7 @@ const Blog = props => {
   return (
     <Layout>
       <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
     </Layout>
   )
 }
